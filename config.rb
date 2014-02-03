be3b4f25a48c196af04cd1abc2dcec354e14c009
Lev Tsypin <lev@thinkshout.com>
2014-02-03 09:51:24 -0800

    Changing CSS compilation directory to assets/stylesheets and moving static syntax.css file into there.

--- config.rb
+++ config.rb
@@ -4,13 +4,14 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "assets/stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "assets/images"
+javascripts_dir = "assets/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
