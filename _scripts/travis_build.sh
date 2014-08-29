ad04362896a382897af7a0f5079fbbd808dc565b
David Lanier <david@thinkshout.com>
2014-08-29 15:30:44 -0700

    fixed syntax in a bash if.

--- _scripts/travis_build.sh
+++ _scripts/travis_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'master' || $TRAVIS_BRANCH == 'live']] ; then
+if [[ $TRAVIS_BRANCH == 'master' || $TRAVIS_BRANCH == 'live' ]]; then
   bundle exec rake build
 else
   echo 'Invalid branch. You can only deploy from master and live.'
