95de430dbf7ba7c3c57993b9add5eb1487f58dbb
Lev Tsypin <lev@thinkshout.com>
2014-09-04 15:50:31 -0700

    Cleaning up Travis config and scripts

--- _scripts/travis_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_BRANCH == 'master' || $TRAVIS_BRANCH == 'live' ]]; then
-  bundle exec rake build
-else
-  echo 'Invalid branch. You can only deploy from master and live.'
-  exit 1
-fi
