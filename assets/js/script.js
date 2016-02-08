d9211e8437d9e31ad1d36b6b99f26b4c8f6e0e4e
amyvs <amy@thinkshout.com>
2016-02-08 13:18:21 -0800

    Additional work done on the main-menu navbar elements and responsiveness, including javascript to slide the menu in and out, and switching the mobile menu icon from bars to an ‘x’.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 (function($) {
   $('.mobile-menu-icon').click(function() {
-    $('.main-menu').fadeToggle();
+    $('header nav .main-menu').slideToggle(800);
+    $(this).find('.fa').toggleClass('fa-bars fa-remove');
   });
 })(jQuery);
