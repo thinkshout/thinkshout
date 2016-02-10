eb55dde7f8533eefaed2b486fcaaf1923de73f4f
amyvs <amy@thinkshout.com>
2016-02-10 15:39:45 -0800

    Apply jQuery waypoints functionality to the three-case-studies blocks
    on the home page, so the styling transitions well for mobile and tablet
    devices.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,16 @@
     $('header nav .main-menu').slideToggle(800);
     $(this).find('.fa').toggleClass('fa-bars fa-remove');
   });
+
+  // Waypoints
+  $('.case-study').each(function() {
+    var current_item = $(this);
+    $(this).waypoint(function(direction) {
+      if (direction == 'down') {
+        current_item.addClass('active');
+      } else {
+        current_item.removeClass('active');
+      }
+    });
+  })
 })(jQuery);
