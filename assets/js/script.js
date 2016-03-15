6ccf6156e0c808d30fcec2b8e0da527f14893357
Joe Komenda <komejo@gmail.com>
2016-03-15 13:20:32 -0700

    Ensure external links open in new tab, remove

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,5 +87,11 @@
       }
     });
 
+  $('a').each(function() {
+     var a = new RegExp('/' + window.location.host + '/');
+     if (!a.test(this.href)) {
+        $(this).attr("target","_blank");
+     }
+  });
 
 })(jQuery);
